f759e995465b39c5754b1a46dec5060a69fdee8b
HARENDRA PRATAP SINGH <harendrapratap194@gmail.com>
2023-12-19 17:13:45 +0530

    initial commit

---
+++ 01.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    int a[3][3];
+    int i,j;
+    /*printf("enter a matrix value : ");
+    for (i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("matrix : \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",&a[i][j]);
+        }
+        printf("\n");
+    }*/
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i==j)
+            {
+                printf("1 ");
+
+            }
+            else
+            {
+                printf("0 ");
+            }
+            
+        }
+        printf("\n");
+    }
+
+}
